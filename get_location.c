aa7e0524bb897958b4f247f2d6f8ab61024e607f
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-11-14 12:30:52 -0800

    location

--- get_location.c
+++ get_location.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * get_path - function to get the path of the file
+ * @token: token for the command
+ * @command: command enter by the user
+ *
+ * Return: path_file
+ */
+
+char *get_path(char *token, char *command)
+{
+	int dir_length = strlen(token);
+	int command_length = strlen(command);
+	char *path_file = malloc(command_length + dir_length + 2);
+
+	strcpy(path_file, token);
+	strcat(path_file, "/");
+	strcat(path_file, command);
+	strcat(path_file, "\0");
+	return (path_file);
+}
+
+/**
  * get_location - a function that tack the location
  * @command: is the char that will execute command
  * Return: NULL.
@@ -22,7 +43,7 @@ char *get_location(char *command)
 		while (token != NULL)
 		{
 			path_file = get_path(token, command);
-			if (stat(path_file, buffer) == 0)
+			if (stat(path_file, &buffer) == 0)
 			{
 				free(path_copy);
 				return (path_file);
